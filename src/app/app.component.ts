a63385b4390455aafe496f77eea51375bf1c46c1
mohamedelmancy <mohamedelmancy998@gmail.com>
2018-04-11 00:12:04 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { HomeService } from './home/home.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  name  = 'mohamed elmancy';
+  obj = { name }; // Object Short Notation
+  constructor(private homeService: HomeService) {}
+  ngOnInit() {
+      console.log('your object ' , this.obj);
+      this.homeService.alerts();
+      }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
+import {HttpModule} from '@angular/http';
+import {CheckUserService} from './main-layout/checkUser';
+import {LoginService, LoginGuard} from './authentication/login/login.service';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from  './authentication/login/login.component';
+import { AppRouting } from './app.routing'; // Added here
+import { HomeService } from './home/home.service';
+// import { TooltipModule } from 'angular2-tooltips';
+import { RouterModule} from '@angular/router';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { ResetPasswordComponent } from './authentication/reset-password/reset-password.component';
+import { LaddaModule } from 'angular2-ladda';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LandingGuard } from './landing-page/landing-guard';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import {RegisterComponent} from './authentication/register/register.component';
+import {BusyModule} from 'angular2-busy';
+import {TooltipModule} from 'ngx-bootstrap/tooltip';
+// import {AccordionModule, BsDropdownModule, CarouselModule, ModalModule, TooltipModule} from 'ngx-bootstrap';
+import { AudioFilesComponent } from './audio-files/audio-files.component';
+import {SpeechRecognitionService} from './main-layout/speech-recognition-service.service';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    LoginComponent,
+    AuthenticationComponent,
+    ResetPasswordComponent,
+    LandingPageComponent,
+    MainLayoutComponent,
+    RegisterComponent,
+    AudioFilesComponent
+    ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRouting,
+    HttpModule,
+    TooltipModule,
+    LaddaModule,
+    ReactiveFormsModule,
+    Angular2FontawesomeModule,
+    BusyModule,
+    FormsModule,
+
+    // ModalModule.forRoot(),
+    // BsDropdownModule.forRoot(),
+    // AccordionModule.forRoot(),
+    TooltipModule.forRoot(),
+    // CarouselModule.forRoot(),
   ],
-  providers: [],
+  providers: [LoginComponent,
+     RouterModule, HomeService, SpeechRecognitionService, LoginGuard, CheckUserService, LandingGuard , LoginService, RegisterComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
